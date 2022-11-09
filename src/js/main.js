f70ae045f6217473e11eb28c7b3da7d06a92188d
Eugene Babenko <eug.babenko@gmail.com>
2022-11-09 17:15:57 +0200

    Added controller, dom, model, view files

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,12 @@
 import '../scss/styles.scss';
+import DOM from './dom.js';
+import View from './view.js';
+import Controller from './controller.js';
+import Model from './model.js';
 
-import DOM from "./dom.js"
-import GameLogic from "./gamelogic.js"
+const dom = new DOM();
+const view = new View();
+const controller = new Controller();
+const model = new Model();
 
-
-
-const dom = new DOM (4, 4);
-const gameLogic = new GameLogic()
-
-window.onload = function () {
-  dom.startGame();
-};
-
-document.addEventListener('keyup', (e) => {
-    if (e.code === "ArrowLeft") {
-        gameLogic.slideLeft();
-        dom.setTwo();
-    }
-    else if (e.code === "ArrowRight") {
-        gameLogic.slideRight();
-        dom.setTwo();
-    }
-    else if (e.code === "ArrowUp") {
-        gameLogic.slideUp();
-        dom.setTwo();
-
-    }
-    else if (e.code === "ArrowDown") {
-        gameLogic.slideDown();
-        dom.setTwo();
-    }
-    document.getElementById("score").innerText = gamelogic.score;
-})
+controller.run()
